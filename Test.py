2f632a39e61454c667563a838a098c0785439beb
JJEM-DE <38837907+JJEM-DE@users.noreply.github.com>
2018-10-05 19:19:51 +0100

    Initial commit

---
+++ Test.py
@@ -0,0 +1,7 @@
+
+limit_tetris = {1:0,2:0,5:0}
+
+while not all(value == 0 for value in limit_tetris.values()):
+	print("Yay")
+
+print("success")
---
+++ main.py
@@ -0,0 +1,28 @@
+# Josh Moody Computing 2
+# Created 05/10/2018
+#Last edit 05/10/2018
+
+
+
+from copy import deepcopy  # copy 'target' to avoid modifying it
+import utils  # it might be helpful to use 'utils.py'
+
+
+def Tetris(target, limit_tetris):
+
+	#blank solution:
+
+	M = target
+	for i in range(len(M)):
+		for j in range(len(M[0])):
+			M[i][j] = (0, 0)
+
+	while not all(value == 0 for value in limit_tetris.values()):    ##loops as long as there are still pieces left to place
+		for key in limit_tetris:         #for each type of piece in the dictionary
+			if limit_tetris[key] != 0:		#if there's a piece of that type left then...
+				M[0][]
+
+
+
+	return M
+
