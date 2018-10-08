bf1317a4ceea7ff815c18dd44207b25f0b7af633
JJEM-DE <38837907+JJEM-DE@users.noreply.github.com>
2018-10-08 12:44:41 +0100

    Fully functional bad algorithm
    Works well, but would only get a 50% mark

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import utils  # it might be helpful to use 'utils.py'
 def Tetris(target, limit_tetris):
 
 	n = 1 #piece ID
+	shapeID = 0
 
 
 	#blank solution:
@@ -35,39 +36,54 @@ def Tetris(target, limit_tetris):
 		for key in limit_tetris:         #for each type of piece in the dictionary
 			print("Key: {0}".format(key))
 			if limit_tetris[key] != 0:		#if there's a piece of that type left then...
-				print("Wooo!!! I'm here! The key is {0}. n = {1}".format(key, n))
+				print("Wooo!!! I'm here! The key is {0}. n = {1}.".format(key, n))
 				print(limit_tetris)
 				shape = utils.generate_shape(key)
 				for i in range(len(M)):     #check each space in the grid
 					for j in range(len(M[0])):   #check each space in the grid
-						print("I'm here! The key is {0}, i = {1} and j = {2}. n = {3}".format(key, i, j, n))
+						print("I'm here! The key is {0}, i = {1} and j = {2}. PieceID = {3}. ShapeID = {4}".format(key, i, j, n, shapeID))
 						if target[i][j] == 1 and limit_tetris[key] !=0 :
 							print("I've got here now!")
 							try:        # To fix index error
 								if target[i+shape[1][0]][j+shape[1][1]] == 1 and target[i+shape[2][0]][j+shape[2][1]] == 1 and target[i+shape[3][0]][j+shape[3][1]] == 1  :      #check if the shape fits
+									print("Results from checking if shape fits: ({0}, {1}), ({2}, {3}), ({4}, {5}), ({6}, {7})".format(i, j, i+shape[1][0], j+shape[1][1], i+shape[2][0], j+shape[2][1], i+shape[3][0], j+shape[3][1]))
+									if j+shape[1][1] < 0 or j+shape[2][1] < 0 or j+shape[2][1] < 0 :
+										print("Negative index error")
+										break
+
+									###debugging
+
+
+									shapeID = limit_tetris[key]
 
 
 
 
-										#place shape into M:
-										M[i][j] = (key, n)
-										M[i+shape[1][0]][j+shape[1][1]] = (key, n)
-										M[i + shape[2][0]][j + shape[2][1]] = (key, n)
-										M[i+shape[3][0]][j+shape[3][1]] = (key, n)
+									###end of debugging
 
 
-										n += 1   #increase shapeID
 
-										#remove the 1s from target:
 
-										target[i][j] = 0
-										target[i + shape[1][0]][j + shape[1][1]] = 0
-										target[i + shape[2][0]][j + shape[2][1]] = 0
-										target[i + shape[3][0]][j + shape[3][1]] = 0
 
-										#remove the piece from the limit_tetris
-										print(limit_tetris[key])
-										limit_tetris[key] = limit_tetris[key] - 1
+									#place shape into M:
+									M[i][j] = (key, n)
+									M[i+shape[1][0]][j+shape[1][1]] = (key, n)
+									M[i + shape[2][0]][j + shape[2][1]] = (key, n)
+									M[i+shape[3][0]][j+shape[3][1]] = (key, n)
+
+
+									n += 1   #increase pieceID
+
+									#remove the 1s from target:
+
+									target[i][j] = 0
+									target[i + shape[1][0]][j + shape[1][1]] = 0
+									target[i + shape[2][0]][j + shape[2][1]] = 0
+									target[i + shape[3][0]][j + shape[3][1]] = 0
+
+									#remove the piece from the limit_tetris
+									print(limit_tetris[key])
+									limit_tetris[key] = limit_tetris[key] - 1
 
 							except IndexError:
 								pass
